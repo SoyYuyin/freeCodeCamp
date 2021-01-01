9424472a181fc8c8e0a31cdaa32930c2695084c7
SoyYuyin <emtzhd@gmail.com>
2021-01-01 10:31:36 -0600

    smallest common multiple and drop it algorithm

---
+++ 14-smallest-common-multiple.js
@@ -0,0 +1,33 @@
+// Smallest Common Multiple
+
+
+
+// Find the smallest common multiple of the provided parameters 
+// that can be evenly divided by both, as well as by all sequential 
+// numbers in the range between these parameters.
+
+// The range will be an array of two numbers that will not 
+// necessarily be in numerical order.
+
+// For example, if given 1 and 3, find the smallest common 
+// multiple of both 1 and 3 that is also evenly divisible by all numbers 
+// between 1 and 3. The answer here would be 6.
+
+
+function smallestCommons(arr) {
+  let low = Math.min(...arr)
+  let high = Math.max(...arr)
+
+  let scm=high
+
+  for(let i=high-1; i>=low; i--){
+      if(scm%i){
+        scm += high
+        i=high
+      }
+  }
+  return scm
+}
+
+
+smallestCommons([1,5]);
