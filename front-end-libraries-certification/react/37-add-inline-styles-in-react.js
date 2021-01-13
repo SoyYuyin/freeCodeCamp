2951475cd0d83b3e12d50e93913d62840d4e108a
SoyYuyin <emtzhd@gmail.com>
2021-01-12 21:00:34 -0600

    react progress 37-41

--- front-end-libraries-certification/react/37-add-inline-styles-in-react.js
+++ front-end-libraries-certification/react/37-add-inline-styles-in-react.js
@@ -7,3 +7,19 @@
 
 // If you have a large set of styles, you can assign a style object to a constant to keep your code organized. Initialize a styles constant and assign an object with three style properties and their values to it. Give the div a color of "purple", a font-size of 40, and a border of "2px solid purple". Then set the style attribute equal to the styles constant.
 
+const styles = {
+    color:"purple",
+    fontSize:40,
+    border:"2px solid purple"
+    }
+  
+  // Change code above this line
+  class Colorful extends React.Component {
+    render() {
+      // Change code below this line
+      return (
+        <div style={styles}>Style Me!</div>
+      );
+      // Change code above this line
+    }
+  };
